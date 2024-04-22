744ee8023716dfd2415ae20bf1498ae6e8e3cb3d
4rchi-nv <arslan0agajanov@gmail.com>
2024-04-22 17:53:33 +0500

    post

--- src/assets/js/post.js
+++ src/assets/js/post.js
@@ -1,37 +1,3 @@
-// const form = document.forms['sargyt'];
-// const nameInput = form.elements['name'];
-// const surnameInput = form.elements['surname'];
-// const phoneNumberInput = form.elements['phonenumber'];
-// const homeNumberInput = form.elements['homenumber'];
-// const emailInput = form.elements['email'];
-// const messageInput = form.elements['message'];
-// async function getNews() {
-//     return response = await fetch("https://api.escuelajs.co/api/v1/products", {
-//         method: "POST",
-//         headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//           },
-//         body: JSON.stringify({
-//             "title": nameInput.value,
-//             "price": homeNumberInput.value,
-//             "description": messageInput.value,
-//             "categoryId": phoneNumberInput.value,
-//             "images": ["https://placeimg.com/640/480/any"]
-//           })
-//     }); 
-// }
-
-// const news = getNews();
-
-
-// function renderNews() {
-//     news
-//         .then(res => res.json())
-//         .then(log => console.log(log))
-//         .catch(err => console.log(err))
-// }
-// form.addEventListener('submit', renderNews);
-
 const form = document.forms['sargyt'];
 const nameInput = form.elements['name'];
 const surnameInput = form.elements['surname'];
@@ -39,21 +5,24 @@ const phoneNumberInput = form.elements['phonenumber'];
 const homeNumberInput = form.elements['homenumber'];
 const emailInput = form.elements['email'];
 const messageInput = form.elements['message'];
+const loading = form.querySelector('.loading');
+const success = form.querySelector('.sent-message');
+const errmsg = form.querySelector('.error-message');
 
 async function submitForm(event) {
   event.preventDefault(); // Prevent default form submission behavior
-  const body = {
-    "name": nameInput.value,
-    "surname": surnameInput.value,
-    "email": emailInput.value,
-    "phone_number": Number(phoneNumberInput.value),
-    "home_number": Number(homeNumberInput.value),
-    "description": messageInput.value,
-  }
-  console.log(body);
-
+  loading.style.display = 'block';
   try {
-    const response = await fetch("http://localhost:80/api/application", {
+    const body = {
+      "name": nameInput.value,
+      "surname": surnameInput.value,
+      "email": emailInput.value,
+      "phone_number": phoneNumberInput.value,
+      "home_number": homeNumberInput.value,
+      "description": messageInput.value,
+    }
+
+    const response = await fetch("http://localhost:80/api/application/", {
       method: "POST",
       headers: {
         'Content-Type': 'application/json; charset=UTF-8',
@@ -62,13 +31,19 @@ async function submitForm(event) {
     });
 
     if (!response.ok) {
+      loading.style.display = 'none';
+      errmsg.style.display = 'block';
       throw new Error(`Error submitting form: ${response.statusText}`);
     }
+    success.style.display = 'block';
 
     const responseData = await response.json();
     console.log("Form submission successful:", responseData);
-    // Handle successful submission (e.g., display a success message)
+    loading.style.display = 'none';
+    form.reset();
   } catch (error) {
+    loading.style.display = 'none';
+    errmsg.style.display = 'block';
     console.error("Error submitting form:", error);
     // Handle errors appropriately (e.g., display an error message)
   }
